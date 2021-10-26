626a16d952f7bf234fb3744c21835daf609fe2bb
hannahellis4242 <hannahellis4242@gmail.com>
2021-10-26 23:52:59 +0100

    shuffling decks

--- src/data.ts
+++ src/data.ts
@@ -1,18 +1,23 @@
+import { Random } from "random";
+
 export class index {
   constructor(public num: number, public reversed: boolean) {}
 }
 
-export default class deck_data {
-  public deck: index[];
-  constructor() {
-    this.deck = new Array(78).fill(0).map((_, i) => new index(i, false));
-  }
-}
+export let createDeck = (): index[] =>
+  new Array(78).fill(0).map((_, i) => new index(i, false));
 
-export let shuffle = (deck: deck_data, i: number): deck_data => {
-  if (i < 0 || i > 77) {
-    return deck;
-  }
-  //TODO
-  return deck;
+export let shuffle = (deck: index[], rng: Random): index[] => {
+  const pools = rng.int(2, 12);
+  return deck
+    .map((v: index, _) => {
+      return {
+        num: v.num,
+        reversed: v.reversed != rng.boolean(),
+        pool: rng.int(0, pools),
+      };
+    })
+    .sort((a, b) => {
+      return a.pool - b.pool;
+    });
 };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-import deck_data, { index } from "./data";
+import { createDeck, shuffle } from "./data";
 import { toCard } from "./utils";
+import random from "random";
+import seedrandom from "seedrandom";
 
-const deck = new deck_data();
-console.log(deck.deck.map((value: index, _) => toCard(value.num)));
+const seed = "Hello World";
+const rng = random.clone(seedrandom(seed));
+
+let deck = createDeck();
+
+const times = rng.int(20, 50);
+for (let i = 0; i < times; i++) {
+  deck = shuffle(deck, rng);
+}
+console.log(
+  deck.map((i, _) => {
+    return { card: toCard(i.num), reversed: i.reversed };
+  })
+);
