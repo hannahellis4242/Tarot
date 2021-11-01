0008937612e2fb4bb823ac908224c8b112f1ed95
Hannah Ellis <hannahellis4242@gmail.com>
2021-11-01 00:36:54 +0000

    making main window not show till dom is ready

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,11 +1,19 @@
 import { app, BrowserWindow } from "electron";
+import path from "path";
 
 let win: BrowserWindow | null = null;
 let createWindow = () => {
-  win = new BrowserWindow();
-  win.maximize();
+  win = new BrowserWindow({ show: false });
   win.on("close", () => {
     win = null;
+  });
+  win.loadFile(path.join(__dirname, "../../render/index.html"));
+  //win.webContents.openDevTools();
+  win.webContents.on("dom-ready", () => {
+    if (win) {
+      win.maximize();
+      win.show();
+    }
   });
 };
 
