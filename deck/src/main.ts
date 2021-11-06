8940e6e07dc22b3c1fafa21efa6275242a4f7ce6
hannahellis4242 <hannahellis4242@gmail.com>
2021-11-06 20:21:22 +0000

    adding an environment variable to set the port

--- deck/src/main.ts
+++ deck/src/main.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import { handleRequest } from "./handler";
 
-const port = 3000;
+const getPort = (d: number): number => {
+  if (process.env.PORT) {
+    return Number(process.env.PORT);
+  }
+  return d;
+};
+
+const port = getPort(5000);
 const app = express();
 app.use(express.json());
 
