d6397ca9bc0bdf66017d522aa97b4e63891cfae0
Hannah Ellis <hannahellis4242@gmail.com>
2021-11-10 01:30:42 +0000

    making script add the contents of the page

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const main = document.createElement("main");
+{
+  const header = document.createElement("header") as HTMLHeadingElement;
+  header.classList.add("page-title");
+  header.textContent = "Tarot Practice";
+  main.appendChild(header);
+}
+{
+  const start = document.createElement("span") as HTMLSpanElement;
+  start.classList.add("button");
+  start.classList.add("centre");
+  start.textContent = "Start";
+  start.addEventListener("click", (e) => {
+    location.href = "/start";
+  });
+  main.appendChild(start);
+}
+document.body.appendChild(main);
