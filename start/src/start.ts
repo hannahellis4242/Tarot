fcd4b134f34f40e431cb8df5d793f96a7e0955b0
Hannah Ellis <hannahellis4242@gmail.com>
2021-11-10 03:08:59 +0000

    example of adding svg

--- start/src/start.ts
+++ start/src/start.ts
@@ -4,4 +4,23 @@ const main = document.createElement("main");
   test.textContent = "test";
   main.appendChild(test);
 }
+{
+  const div = document.createElement("div");
+  const ns = "http://www.w3.org/2000/svg";
+  const svg = document.createElementNS(ns, "svg");
+  svg.setAttributeNS(null, "width", "100%");
+  svg.setAttributeNS(null, "height", "100%");
+  div.appendChild(svg);
+  const rect = document.createElementNS(ns, "rect");
+  rect.setAttributeNS(null, "width", "100");
+  rect.setAttributeNS(null, "height", "100");
+  rect.setAttributeNS(null, "fill", "#f06");
+  svg.appendChild(rect);
+  const img = document.createElementNS(ns, "image");
+  img.setAttributeNS(null, "href", "cards/0.jpg");
+  img.setAttributeNS(null, "width", "100");
+  svg.appendChild(img);
+  div.appendChild(svg);
+  main.appendChild(div);
+}
 document.body.appendChild(main);
