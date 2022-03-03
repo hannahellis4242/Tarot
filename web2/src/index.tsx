7a1c1078f3e3ad5a76e3b8355e9c20d940530e01
Hannah Ellis <hannahellis4242@gmail.com>
2022-03-03 20:51:25 +0000

    Adding App file

--- web2/src/index.tsx
+++ web2/src/index.tsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom";
-//import App from "./App";
+import App from "./App";
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
