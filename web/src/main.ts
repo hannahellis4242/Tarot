14acd7d6167c26288f2ec532cce861fce6a93e7b
hannahellis4242 <hannahellis4242@gmail.com>
2022-11-01 23:45:34 +0000

    redirecting to internal severs

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,104 +1,31 @@
-import axios, { AxiosResponse } from "axios";
 import express from "express";
-import path from "path";
-import morgan from "morgan";
-import ServerInformation from "./ServerInformation";
+import { createProxyServer } from "http-proxy";
 
-const serverInfo = new ServerInformation();
-
-const web = serverInfo.get("web").unwrap();
-const deckServer = serverInfo.get("deck").unwrap();
-const wordServer = serverInfo.get("word").unwrap();
+const getServerInfo = (name: string, defaultPort: number) => {
+  const hostEnv = process.env[`${name}-host`];
+  const portEnv = process.env[`${name}-port`];
+  return {
+    host: hostEnv || "localhost",
+    port: portEnv || defaultPort.toString(),
+  };
+};
 
 const app = express();
-app.use(express.json());
-app.use(morgan("combined"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "..", "public")));
+const client = getServerInfo("client", 3000);
+const deck = getServerInfo("deck", 5000);
+const word = getServerInfo("word", 5001);
 
-app.get("/", (_, res) => {
-  res.render("pages/index", { title: "Tarot" });
+//set up proxy
+const proxy = createProxyServer();
+app.use("/deck", (req, res, next) => {
+  proxy.web(req, res, { target: `http://${deck.host}:${deck.port}` }, next);
 });
-
-app.get("/index", (_, res) => {
-  res.render("pages/index", { title: "Tarot" });
+app.use("/word", (req, res, next) => {
+  proxy.web(req, res, { target: `http://${word.host}:${word.port}` }, next);
 });
-
-app.get("/instructions", (_, res) => {
-  res.render("pages/instructions", { title: "Instructions" });
+app.use("/", (req, res, next) => {
+  proxy.web(req, res, { target: `http://${client.host}:${client.port}` }, next);
 });
-
-app.get("/tarot", (_, res) => {
-  res.render("pages/tarot", { title: "Tarot" });
-});
-
-app.get("/deck-generator", (_, res) => {
-  res.render("pages/deck-generator", { title: "Deck Input" });
-});
-
-app.get("/deck", (req, res) => {
-  axios
-    .get(
-      "http://" + deckServer.host + ":" + deckServer.port.toString() + "/deck",
-      {
-        params: { seed: req.query.seed, draw: req.query.draw },
-      }
-    )
-    .then((value) => {
-      if ("err" in value.data) {
-        console.log("error :", value.data.err);
-        res.status(200).send("Error");
-      } else {
-        res.render("pages/deck-result", { title: "Cards", cards: value.data });
-        //res.status(200).json(value.data);
-      }
-    })
-    .catch((reason) => {
-      console.log("catch : ", reason);
-      res.status(200).send(JSON.stringify(reason));
-    });
-});
-
-app.get("/word", (req, res) => {
-  axios
-    .get(
-      "http://" +
-        wordServer.host +
-        ":" +
-        wordServer.port.toString() +
-        "/random",
-      {
-        params: { num: 100 },
-      }
-    )
-    .then((value) => {
-      if ("err" in value.data) {
-        console.log("error :", value.data.err);
-        res.status(200).send("Error");
-      } else {
-        const chunks: string[][] = [];
-        const rows = 20;
-        const cols = 5;
-        for (let i = 0; i < rows; ++i) {
-          const chunk: string[] = [];
-          for (let j = 0; j < cols; ++j) {
-            chunk.push(value.data.words[i * cols + j]);
-          }
-          chunks.push(chunk);
-        }
-        res.render("pages/seed-word", { title: "word seed", rows: chunks });
-      }
-    })
-    .catch((reason) => {
-      console.log("catch : ", reason);
-      res.status(200).json(reason);
-    });
-});
-
-app.get("/seed", (req, res) => {
-  res.render("pages/seed", { title: "Seeds" });
-});
-
-app.listen(web.port, web.host, () => {
-  console.log(`listening at http://${web.host}:${web.port}`);
+app.listen(8080, "0.0.0.0", () => {
+  console.log("web server started on port 8080");
 });
--- web/src/utils/getProperty.ts
+++
@@ -1,4 +0,0 @@
-const getProperty = <T, K extends keyof T>(o: T, propertyName: K): T[K] => {
-  return o[propertyName]; // o[propertyName] is of type T[K]
-};
-export default getProperty;
