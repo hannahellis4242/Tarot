8d0ab5de808e29305be78b499b63d5578771cf8b
hannahellis4242 <hannahellis4242@gmail.com>
2022-11-03 22:45:12 +0000

    Tidying up components some, plus fixed broken copy button. Reordered components also

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,26 @@
-import Generator from "./Generator";
+import ResultContextProvider from "../store/ResultContext";
+import SelectedWordsContextProvider from "../store/SelectedWordsContext";
+import WordsContextProvider from "../store/WordsContext";
+import GeneratorInput from "./GeneratorInput";
+import GeneratorResult from "./GeneratorResult";
+import Instructions from "./Instructions";
+import WordInput from "./WordInput";
 import WordsList from "./WordsList";
 
 const App = () => (
   <section>
-    <Generator />
-    <WordsList />
+    <header>Deck Generator</header>
+    <Instructions />
+    <SelectedWordsContextProvider>
+      <WordsContextProvider>
+        <WordInput />
+        <WordsList />
+        <ResultContextProvider>
+          <GeneratorInput />
+          <GeneratorResult />
+        </ResultContextProvider>
+      </WordsContextProvider>
+    </SelectedWordsContextProvider>
   </section>
 );
 export default App;
---
+++ client/src/components/Instructions.tsx
@@ -0,0 +1,16 @@
+const Instructions = () => (
+  <section>
+    <p>To "shuffle and cut" the cards you may etiher</p>
+    <ul>
+      <li>select at least 4 meaningful words from the options below</li>
+      <li>or you may type your own seed in to the seed input box</li>
+    </ul>
+    <p>
+      Then select the number of cards to draw from the deck, or if left blank
+      will generate the full deck.
+    </p>
+    <p>Once you are ready hit the submit button</p>
+  </section>
+);
+
+export default Instructions;
